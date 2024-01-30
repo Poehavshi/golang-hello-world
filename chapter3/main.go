884f9fdfa774128bedc0a36ada668bbeff246367
arkadiysotnikov <cotnikoarkady@gmail.com>
2024-01-30 16:51:00 +0400

    feat(chapter3): variables declaration

--- chapter3/main.go
+++ chapter3/main.go
@@ -3,9 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1 + 1=", 1+1)
-	fmt.Println("1.1 + 1.1 =", 1.1+1.1)
+	var a int
+	var b int
+	a = 1
+	b = 1
+	fmt.Println("a + b=", a+b)
+	var c float64
+	var d float64
+	fmt.Println("1.1 + 1.1 =", c+d)
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[1])
 	fmt.Println("Hello " + "World")
+	var helloWorld string = "Hello world"
+	fmt.Println(helloWorld)
+
+	newString := "string"
+	fmt.Println(newString)
 }
